1695a989564fe0835acb5bc4e02122a1e5d7ff4b
Weidong Cai <cai.1836167@studenti.uniroma1.it>
2023-05-23 23:58:21 +0200

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'screen/chat.dart';
+import 'screen/dashboard.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int currentTab = 0;
+  final List<Widget> screens = [
+    Dashboard(),
+    Chat()
+  ];
+
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = Dashboard();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageStorage(
+        child: currentScreen,
+        bucket: bucket,
+      ),
+      floatingActionButton: FloatingActionButton( //add button
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              //left bluetooth
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 60,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Dashboard();
+                        currentTab = 0;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.bluetooth,
+                          color: currentTab == 0 ? Colors.blueAccent : Colors.grey,
+                        ),
+                        Text(
+                          'BLE',
+                          style: TextStyle(color: currentTab == 0 ? Colors.blueAccent : Colors.grey),
+                        )
+                      ],
+                    ),
+                    )
+                ],
+              ),
+              //right history
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 60,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Chat();
+                        currentTab = 1;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.history,
+                          color: currentTab == 1 ? Colors.blueAccent : Colors.grey,
+                        ),
+                        Text(
+                          'History',
+                          style: TextStyle(color: currentTab == 1 ? Colors.blueAccent : Colors.grey),
+                        )
+                      ],
+                    ),
+                    )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
